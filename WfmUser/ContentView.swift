ce1b566e04ca6113aae545aed66aeaadff9816af
Alper <alperaydin@onuss.com>
2025-03-13 16:59:19 +0300

    Initial Commit

---
+++ WfmUser/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WfmUser
+//
+//  Created by Mustafa Alper Aydin on 13.03.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WfmUser/WfmUserApp.swift
@@ -0,0 +1,17 @@
+//
+//  WfmUserApp.swift
+//  WfmUser
+//
+//  Created by Mustafa Alper Aydin on 13.03.2025.
+//
+
+import SwiftUI
+
+@main
+struct WfmUserApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
