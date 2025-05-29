2d648c03b68d4099e5579681a4626be5545ffa23
Alper <alperaydin@onuss.com>
2025-05-29 16:30:44 +0300

    feat: codacytest123456

--- WfmUser/ContentView.swift
+++ WfmUser/ContentView.swift
@@ -24,50 +24,36 @@ struct ContentView: View {
 }
 
 
-func complexFunctionxxx(value: Int, flag: Bool) -> Int {
-    
-    var xxxx = Int("STR")!
-    
-    if value == 0 {
-        return "Sıfır1"
-    }
-    
+func complicatedFunction(_ value: Int) -> Int {
+    var result = 0
     if value > 0 {
-        if flag {
-            for i in 0..<value {
-                if i % 2 == 0 {
-                    print("Çift1: \(i)")
-                } else {
-                    print("Tek1: \(i)")
-                }
+        for i in 0..<value {
+            if i % 2 == 0 {
+                result += i * 2
+            } else if i % 3 == 0 {
+                result += i * 3
+            } else if i % 5 == 0 {
+                result += i * 5
+            } else {
+                result += i
             }
-            switch value {
-            case 1:
-                return "Bir1"
-            case 2, 3:
-                return "İki veya Üç1"
-            case 4...10:
-                return "Dört ile On Arasında1"
-            default:
-                return "Diğer1"
-            }
-        } else {
-            while value > 0 {
-                print("Value:1 \(value)")
-                break
-            }
-            return "Pozitif1 ama flag false"
         }
+    } else if value == 0 {
+        result = 42
     } else {
-        guard flag else {
-            return "Negatif1 ve flag false"
-        }
-        if value < -10 {
-            return "Çok Negatif1"
-        } else if value < -5 {
-            return "Orta Negati1f"
-        } else {
-            return "Az 1Negatif"
-        }
+        result = -1
     }
+    return result
+}
+
+func forceTryExample() -> String {
+    let data = "123".data(using: .utf8)!
+    let json = try! JSONSerialization.jsonObject(with: data, options: [])
+    return "\(json)"
+}
+
+func greetUser(name: String) -> String {
+    let greeting = "Hello, " + name + "!"
+    print("Hello, " + name + "!")
+    return "Hello, " + name + "!"
 }
